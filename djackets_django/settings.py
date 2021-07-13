8ad152bcaaee318bc5ddcfc42052258bf38925c0
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2021-07-13 17:53:39 +0200

    Fix

--- djackets_django/settings.py
+++ djackets_django/settings.py
@@ -12,15 +12,14 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+from secrets import *
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ggr*_e@snyt57wmqrq66+znr4x%$0sq+6))x2y*6)&1c-&iu16'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
